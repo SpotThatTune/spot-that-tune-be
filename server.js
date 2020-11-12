c538294fee85d7e61c55b4979d06687a5e46eca6
Shawn Carpenter <shawn.carpenter@gmail.com>
2020-11-12 15:05:35 -0800

    sockets working (#1)

--- server.js
+++ server.js
@@ -12,3 +12,27 @@ process.on('exit', () => {
   console.log('Goodbye!');
   pool.end();
 });
+
+// const http = require('http').createServer();
+
+// const io = require('socket.io')(http);
+ 
+// let count = 0;
+ 
+// io.on('connection', socket => {
+//   socket.emit('UPDATED_COUNT', count);
+ 
+//   socket.on('INCREMENT', () => {
+//     count++;
+//     socket.emit('UPDATED_COUNT', count);
+//     socket.broadcast.emit('UPDATED_COUNT', count);
+//   });
+ 
+//   socket.on('DECREMENT', () => {
+//     count--;
+//     socket.emit('UPDATED_COUNT', count);
+//     socket.broadcast.emit('UPDATED_COUNT', count);
+//   });
+// });
+ 
+// http.listen(7890);
